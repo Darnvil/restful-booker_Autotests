1a88da21bfaab23c0713ebf5e847656dbf109d84
Darnvil <kolbasko2014@yandex.ru>
2026-05-03 00:27:31 +0300

    Init repo and added test to see if all works well

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
+    testImplementation("io.rest-assured:rest-assured:6.0.0")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/tests/SmokeTest.kt
@@ -0,0 +1,12 @@
+package tests
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class SmokeTest {
+
+    @Test
+    fun `project is working`() {
+        assertTrue(true)
+    }
+}
