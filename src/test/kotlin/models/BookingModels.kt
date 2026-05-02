03273aaf8433ee6f12e865768c83bc6b9c82f46e
Darnvil <kolbasko2014@yandex.ru>
2026-05-03 00:38:07 +0300

    Added first models for booking and rewrited the test for booking

---
+++ src/test/kotlin/models/BookingModels.kt
@@ -0,0 +1,15 @@
+package models
+
+data class BookingDates(
+    val checkin: String,
+    val checkout: String
+)
+
+data class Booking(
+    val firstname: String,
+    val lastname: String,
+    val totalprice: Int,
+    val depositpaid: Boolean,
+    val bookingdates: BookingDates,
+    val additionalneeds: String
+)
--- src/test/kotlin/tests/BookingTest.kt
+++ src/test/kotlin/tests/BookingTest.kt
@@ -2,20 +2,24 @@ package tests
 
 import config.BaseTest
 import io.restassured.RestAssured.given
+import models.Booking
 import org.hamcrest.Matchers.notNullValue
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class BookingTest: BaseTest() {
 
     @Test
-    fun `get booking and check if name is not null`() {
-        given()
+    fun `get booking and check if firstname is Sally`() {
+        val responce = given()
             .log().ifValidationFails()
             .get("/booking/1")
             .then()
             .log().ifValidationFails()
             .statusCode(200)
-            .body("firstname", notNullValue())
-            .body("lastname", notNullValue())
+            .extract()
+            .`as`(Booking::class.java)
+
+        assertEquals("Sally", responce.firstname)
     }
 }
