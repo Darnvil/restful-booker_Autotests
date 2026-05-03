e7a5543982665fce19cd4e6f26c4e74940fb992c
Darnvil <kolbasko2014@yandex.ru>
2026-05-03 15:20:07 +0300

    Added smoke test for api healthcheck

--- src/test/kotlin/tests/SmokeTest.kt
+++ src/test/kotlin/tests/SmokeTest.kt
@@ -1,12 +1,23 @@
 package tests
 
+import config.BaseTest
+import io.restassured.RestAssured.given
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
-class SmokeTest {
+class SmokeTest : BaseTest() {
 
     @Test
     fun `project is working`() {
         assertTrue(true)
+    }
+
+    @Test
+    fun `api is functional`() {
+        given()
+            .log().ifValidationFails()
+            .get("/ping")
+            .then()
+            .statusCode(201)
     }
 }
