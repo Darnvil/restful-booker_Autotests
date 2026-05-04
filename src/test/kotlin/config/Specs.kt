dfa9dd807510ef412a33e9d09fea2b6737060464
Darnvil <kolbasko2014@yandex.ru>
2026-05-04 03:02:52 +0300

    Added Allure filter to specs

--- src/test/kotlin/config/Specs.kt
+++ src/test/kotlin/config/Specs.kt
@@ -1,5 +1,6 @@
 package config
 
+import io.qameta.allure.restassured.AllureRestAssured
 import io.restassured.builder.RequestSpecBuilder
 import io.restassured.builder.ResponseSpecBuilder
 import io.restassured.filter.log.LogDetail
@@ -7,6 +8,7 @@ import io.restassured.filter.log.LogDetail
 val requestSpec = RequestSpecBuilder()
     .setBaseUri(Config.baseUri)
     .log(LogDetail.URI)
+    .addFilter(AllureRestAssured())
     .build()
 
 val responseSpec = ResponseSpecBuilder()
